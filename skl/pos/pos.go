23ed888ca30cc264206671b69884a4b89f8eb5b8
BaiMeow <baishu2003@outlook.com>
2022-11-01 20:06:01 +0800

    feat: some position in hdu

---
+++ skl/pos/pos.go
@@ -0,0 +1,15 @@
+package pos
+
+type Pos struct {
+	Longitude float64
+	Latitude  float64
+}
+
+var (
+	Building6North  = Pos{120.337400, 30.317000}
+	Building6Middle = Pos{120.337400, 30.316560}
+	Building6South  = Pos{120.337400, 30.3161700}
+	Building7North  = Pos{120.339800, 30.317000}
+	Building7Middle = Pos{120.339250, 30.316750}
+	Building7South  = Pos{120.339800, 30.316600}
+)
