8cf54bca59da30004b343e356c16f99c1042a72c
sslime336 <sslime@yeah.net>
2022-11-15 09:00:50 +0800

    tidy

--- phy/captcha_ocr.go
+++ phy/captcha_ocr.go
@@ -3,7 +3,6 @@ package phy
 import (
 	"bytes"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/hduLib/hdu/client"
@@ -11,14 +10,19 @@ import (
 )
 
 func getCaptchaContent() (string, error) {
-	req, _ := http.NewRequest(http.MethodGet, "http://phy.hdu.edu.cn/captcha.svl", nil)
+	req, err := http.NewRequest(http.MethodGet, "http://phy.hdu.edu.cn/captcha.svl", nil)
+	if err != nil {
+		return "", err
+	}
 
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7")
-	req.Header.Set("Cache-Control", "max-age=0")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+	{
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		req.Header.Set("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7")
+		req.Header.Set("Cache-Control", "max-age=0")
+		req.Header.Set("Connection", "keep-alive")
+		req.Header.Set("Upgrade-Insecure-Requests", "1")
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+	}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -35,7 +39,7 @@ func getCaptchaContent() (string, error) {
 	rd := bytes.NewReader(b)
 	captchaContent, err := ocr.Parse(rd)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	return captchaContent, nil
--- phy/captcha_ocr_test.go
+++ phy/captcha_ocr_test.go
@@ -1,10 +1,14 @@
 package phy
 
 import (
+	"os"
 	"testing"
+
+	"github.com/hduLib/hdu/internal/ocr"
 )
 
 func TestGetCaptchaContent(t *testing.T) {
+	ocr.SetToken(os.Getenv("TOKEN"))
 	_, err := getCaptchaContent()
 	if err != nil {
 		t.Fatal(err)
