7b6f6990d50f4fffe53432f7df9c3202e44864c6
BaiMeow <baishu2003@outlook.com>
2022-11-19 13:58:33 +0800

    fix: wrong check

--- skl/schema/schema.go
+++ skl/schema/schema.go
@@ -16,7 +16,9 @@ type schemaNode struct {
 
 type Schema []schemaNode
 
-var schemaReg = regexp.MustCompile(`星期([一二三四五六日])第(\d)-(\d)节{(.*)}`)
+var schemaReg = regexp.MustCompile(`星期([一二三四五六日])第(\d*)-(\d*)节{(.*)}`)
+var weeksReg = regexp.MustCompile(`(\d*)-(\d*)周(\((.)\))?`)
+
 var weekDayMapping = map[string]time.Weekday{
 	"一": time.Monday,
 	"二": time.Tuesday,
@@ -31,7 +33,7 @@ var weekDayMapping = map[string]time.Weekday{
 // with input [1,12] if there is any course this day.
 func (s Schema) Check(begin, end int, weekday time.Weekday, weekNum int) bool {
 	for _, v := range s {
-		if v.Begin <= begin && v.End >= end && v.Weekday == weekday && v.WeekNum.Check(weekNum) {
+		if v.Begin >= begin && v.End <= end && v.Weekday == weekday && v.WeekNum.Check(weekNum) {
 			return true
 		}
 	}
@@ -78,12 +80,34 @@ func decodeWeeks(str string) (weeks, error) {
 	var w weeks
 	for _, v := range ss {
 		if strings.Contains(v, "-") {
-			var begin, end int
-			_, err := fmt.Sscanf(v, "%d-%d周", &begin, &end)
+			var status int
+			matches := weeksReg.FindAllStringSubmatch(v, -1)
+			if len(matches) != 1 {
+				return w, fmt.Errorf("ErrDecodingWeeks:invalid weeks count")
+			}
+			begin, err := strconv.Atoi(matches[0][1])
 			if err != nil {
-				return w, fmt.Errorf("ErrDecodingWeeks:%v", err)
+				return 0, fmt.Errorf("ErrDecodingWeeks:invalid begin time")
+			}
+			end, err := strconv.Atoi(matches[0][2])
+			if err != nil {
+				return 0, fmt.Errorf("ErrDecodingWeeks:invalid end time")
+			}
+			if len(matches[0]) == 5 {
+				switch matches[0][4] {
+				case "单":
+					status = 0
+				case "双":
+					status = 1
+				default:
+					status = 2
+				}
 			}
 			for begin <= end {
+				if status == begin%2 {
+					begin++
+					continue
+				}
 				w |= 1 << begin
 				begin++
 			}
